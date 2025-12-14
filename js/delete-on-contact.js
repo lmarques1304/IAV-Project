d8a7046d0544b5e3f3d7a309f386ebcf0dad4449
nrimac <nrimac00@gmail.com>
2025-12-14 20:27:37 +0000

    Rework

--- js/delete-on-contact.js
+++ js/delete-on-contact.js
@@ -1,16 +1,16 @@
-AFRAME.registerComponent('delete-on-contact', {
+AFRAME.registerComponent("delete-on-contact", {
   init: function () {
-    this.el.addEventListener('contactbegin', (e) => {
-      const hitEl = e.detail.otherEl;
+    this.el.addEventListener("collide", (e) => {
+      const hitEl = e.detail.body ? e.detail.body.el : null;
 
-      if (hitEl && hitEl.classList.contains('grabbable')) {
+      if (hitEl && hitEl.classList.contains("grabbable")) {
         setTimeout(() => {
           if (hitEl.parentNode) {
             hitEl.parentNode.removeChild(hitEl);
-            this.el.sceneEl.emit('item-deleted');
+            this.el.sceneEl.emit("item-deleted");
           }
         }, 0);
       }
     });
-  }
+  },
 });
