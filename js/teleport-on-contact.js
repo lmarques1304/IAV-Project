a3632f40c56aa8d7410022e6e993dce2132ca172
nrimac <nrimac00@gmail.com>
2025-12-14 18:34:50 +0000

    Small fix

--- js/teleport-on-contact.js
+++ js/teleport-on-contact.js
@@ -1,16 +1,22 @@
 AFRAME.registerComponent("teleport-on-contact", {
   schema: {
     targetUrl: { type: "string" },
-    targetElement: { type: "selector" },
+    targetElement: { type: "string" }, // Changed to 'string' to avoid loading order issues
   },
 
   init: function () {
     this.onBodyEntered = this.onBodyEntered.bind(this);
+    // Listen for collision events
     this.el.addEventListener("body-entered", this.onBodyEntered);
   },
 
   onBodyEntered: function (evt) {
-    if (evt.detail.body.el === this.data.targetElement) {
+    // The element we collided with
+    const hitElement = evt.detail.body.el;
+    
+    // Check if the hit element matches the target selector (e.g., "#magicSphere")
+    if (hitElement.matches(this.data.targetElement)) {
+      console.log("Teleporting to:", this.data.targetUrl);
       window.location.href = this.data.targetUrl;
     }
   },
