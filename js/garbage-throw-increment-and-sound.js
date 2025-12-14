69a8efdf7dc3109a6441a1441f21ed05d6660284
nrimac <nrimac00@gmail.com>
2025-12-14 20:54:50 +0000

    Refactoring throw script

--- js/garbage-throw-increment-and-sound.js
+++ js/garbage-throw-increment-and-sound.js
@@ -1,14 +1,64 @@
-//requires delete-on-contact.js to be loaded first and has to be defered
-//requires #counter element in html andd #garbage-throw-sound sound entity
-let deleteCount = 0;
-const counterEl = document.querySelector("#counter");
+/* File: garbage-throw-increment-and-sound.js
+  Usage: Attach this component to your <a-scene> tag.
+  Example: <a-scene game-manager>
+*/
 
-document.querySelector("a-scene").addEventListener("item-deleted", () => {
-  deleteCount++;
-  counterEl.setAttribute("text", "value", `Items collected: ${deleteCount}/10`);
-  if (deleteCount >= 10) {
+AFRAME.registerComponent("game-manager", {
+  schema: {
+    targetCount: { type: "int", default: 10 },
+    counterId: { type: "string", default: "#counter" },
+    soundId: { type: "string", default: "#garbage-throw-sound" },
+  },
+
+  init: function () {
+    // 1. Initialize State
+    this.deleteCount = 0;
+
+    // 2. Cache References (Do this once, not every frame)
+    // We look for elements inside the scene to be safe
+    this.counterEl = document.querySelector(this.data.counterId);
+    this.soundEl = document.querySelector(this.data.soundId);
+
+    // Check if elements exist to prevent crashes
+    if (!this.counterEl)
+      console.warn("Game Manager: Counter element not found!");
+    if (!this.soundEl) console.warn("Game Manager: Sound element not found!");
+
+    // 3. Bind the event listener to 'this' so we can access variables
+    this.handleItemDeleted = this.handleItemDeleted.bind(this);
+
+    // 4. Listen for the event on the Scene
+    this.el.addEventListener("item-deleted", this.handleItemDeleted);
+  },
+
+  handleItemDeleted: function () {
+    // Increment
+    this.deleteCount++;
+
+    // Update Text
+    if (this.counterEl) {
+      this.counterEl.setAttribute(
+        "text",
+        "value",
+        `Items collected: ${this.deleteCount}/${this.data.targetCount}`
+      );
+    }
+
+    // Play Sound
+    if (this.soundEl && this.soundEl.components.sound) {
+      this.soundEl.components.sound.playSound();
+    }
+
+    // Check Win Condition
+    if (this.deleteCount >= this.data.targetCount) {
+      this.handleLevelComplete();
+    }
+  },
+
+  handleLevelComplete: function () {
     const currentPage = window.location.pathname.split("/").pop();
     let nextPage;
+
     switch (currentPage) {
       case "plastic.html":
         nextPage = "metal.html";
@@ -21,35 +71,17 @@ document.querySelector("a-scene").addEventListener("item-deleted", () => {
         break;
       case "electronic.html":
         window.location.href = "index.html";
-        break;
+        return;
     }
+
     if (nextPage) {
+      console.log("Level Complete! Moving to:", nextPage);
       window.location.href = nextPage;
     }
-  }
+  },
 
-  garbageThrowSound = document.querySelector("#garbage-throw-sound");
-  garbageThrowSound.components.sound.playSound();
+  // Cleanup if the scene is removed (Good practice)
+  remove: function () {
+    this.el.removeEventListener("item-deleted", this.handleItemDeleted);
+  },
 });
-
-// In your HTML, make sure to have the following elements:
-
-//   <a-entity
-//     sound="src: #bin-sound; volume: 0.3 positional: false"
-//     id="garbage-throw-sound"
-//   ></a-entity>
-
-// <a-entity id="counter-display" position="0 1.5 -5" scale="1.5 1.5 1.5">
-//     <a-plane
-//       color="#000000"
-//       opacity="0.5"
-//       width="2"
-//       height="0.5"
-//       position="0 0 -0.1"
-//     ></a-plane>
-//     <a-entity
-//       id="counter"
-//       text="value: Items collected: 0/10; color: #FFFFFF; align: center; width: 3.5"
-//       position="0 0 0"
-//     ></a-entity>
-//   </a-entity>
