105c64353d837fa36911da0fac643cc9491e81d7
nrimac <nrimac00@gmail.com>
2025-12-14 18:12:39 +0000

    First island fix

--- js/teleport-on-contact.js
+++ js/teleport-on-contact.js
@@ -1,11 +1,21 @@
-AFRAME.registerComponent('teleport-on-contact', {
+AFRAME.registerComponent("teleport-on-contact", {
   schema: {
-    targetUrl: {type: 'string'}
+    targetUrl: { type: "string" },
+    targetElement: { type: "selector" },
   },
 
   init: function () {
-    this.el.addEventListener('hitstart', () => {
+    this.onBodyEntered = this.onBodyEntered.bind(this);
+    this.el.addEventListener("body-entered", this.onBodyEntered);
+  },
+
+  onBodyEntered: function (evt) {
+    if (evt.detail.body.el === this.data.targetElement) {
       window.location.href = this.data.targetUrl;
-    });
-  }
+    }
+  },
+
+  remove: function () {
+    this.el.removeEventListener("body-entered", this.onBodyEntered);
+  },
 });
