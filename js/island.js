006a5d6e45f3f1808090ff662767caf9b718c848
nrimac <nrimac00@gmail.com>
2025-12-15 10:26:36 +0000

    Add barriers

--- js/island.js
+++ js/island.js
@@ -1,13 +1,13 @@
 /* island.js */
 
-AFRAME.registerComponent('island-setup', {
+AFRAME.registerComponent("island-setup", {
   init: function () {
     // Wait for the scene to fully load before running setup
     // This fixes the issue where elements might not be ready yet
     if (this.el.hasLoaded) {
       this.setup();
     } else {
-      this.el.addEventListener('loaded', () => {
+      this.el.addEventListener("loaded", () => {
         this.setup();
       });
     }
@@ -27,7 +27,7 @@ AFRAME.registerComponent('island-setup', {
     if (defaultLight) {
       defaultLight.setAttribute("light", {
         type: "ambient",
-        intensity: 1
+        intensity: 1,
       });
     }
 
@@ -36,7 +36,7 @@ AFRAME.registerComponent('island-setup', {
     if (sunLight) {
       sunLight.setAttribute("light", {
         type: "directional",
-        intensity: 0.8
+        intensity: 0.8,
       });
     }
 
@@ -46,5 +46,45 @@ AFRAME.registerComponent('island-setup', {
       // It is often safer to set position as a string or an object
       rig.setAttribute("position", { x: 0, y: 1.6, z: 0 });
     }
-  }
+  },
+});
+
+AFRAME.registerComponent("stay-in-bounds", {
+  schema: {
+    xRange: { type: "number", default: 30 }, // Distance from center
+    zRange: { type: "number", default: 30 },
+  },
+
+  tick: function () {
+    const currentPosition = this.el.getAttribute("position");
+    const limitX = this.data.xRange;
+    const limitZ = this.data.zRange;
+
+    let x = currentPosition.x;
+    let z = currentPosition.z;
+    let modified = false;
+
+    // Check X bounds
+    if (x > limitX) {
+      x = limitX;
+      modified = true;
+    } else if (x < -limitX) {
+      x = -limitX;
+      modified = true;
+    }
+
+    // Check Z bounds
+    if (z > limitZ) {
+      z = limitZ;
+      modified = true;
+    } else if (z < -limitZ) {
+      z = -limitZ;
+      modified = true;
+    }
+
+    // Only update the DOM if we actually hit a wall
+    if (modified) {
+      this.el.setAttribute("position", { x: x, y: currentPosition.y, z: z });
+    }
+  },
 });
